53f28511bb0ca7cf480994012a71296d0fbcfa74
LuizArthur28 <luiz.weiss@escola.pr.gov.br>
2025-09-10 08:34:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const Lanches = [
+    {Id: "tudo", nome: "X-tudo", Preço: 24.99 },
+    {Id: "gaucho", nome: "X-gaúcho", Preço: 23.99 },
+    {Id: "salada", nome: "X-salada", Preço: 23.99 },
+    {Id: "bacon", nome: "x-bacon", Preço: 23.99 },
+    {Id:"frango", nome: "X-frango", Preço: 23.99 },
+    {Id: "pedrao", nome: "x-pedrao", Preço: 29.99 },
+];
+
+
+const Bebidas = [
+    {Id: "uva", nome: "suco de uva", Preço: 10.00 },
+    {Id: "morango", nome: "suco de morango", Preço: 11.99 },
+    {Id: "maracujá", nome: "suco e maracujá", Preço: 12.99 },
+    {Id:"cajú", nome: "suco de cajú", Preço: 10.99 },
+    {Id: "ET", nome: "suco ET", Preço: 999.99 }
+];
+
+
+function preencheOption(){
+const selectLanche = document.getElementById("transporte")
+
+
+}
+
+
+preencheOption();
