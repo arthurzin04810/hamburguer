2f240a14440cf8510c38d6b2af5b96b7d0b05d83
athzin04810 <arthur.hettwer@escola.pr.gov.br>
2025-09-26 09:41:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const Lanches = [
-    {Id: "tudo", nome: "X-tudo", Preço: 27.99 },
-    {Id: "gaucho", nome: "X-gaúcho", Preço: 29.99 },
-    {Id: "salada", nome: "X-salada", Preço: 24.99 },
-    {Id:"egg", nome: "X-egg", Preço: 27.99 },
-    {Id: "bacon", nome: "x-tudo", Preço: 27.99 }
+const lanches = [
+    {Id: "X-tudo", nome: "X-tudo", Preço: 27.99 },
+    {Id: "X-gaÚcho", nome: "X-gaúcho", Preço: 29.99 },
+    {Id: "X-salada", nome: "X-salada", Preço: 24.99 },
+    {Id:"x-pedrao", nome: "X-pdrao", Preço: 27.99 },
+    {Id: "X-bacon", nome: "X-pedrao", Preço: 27.99 }
 ];
 
 
 const Bebidas = [
-    {Id: "uva", nome: "suco de uva", Preço: 10.00 },
-    {Id: "morango", nome: "suco de morango", Preço: 11.99 },
+    {Id: "suco de uva", nome: "suco de uva", Preço: 10.00 },
+    {Id: "suco de laranja", nome: "suco de laranja", Preço: 11.99 },
     {Id: "maracujá", nome: "suco e maracujá", Preço: 12.99 },
-    {Id:"cajú", nome: "suco de cajú", Preço: 10.99 },
-    {Id: "ET", nome: "suco ET", Preço: 999.99 }
+    {Id:"suco de abacaxi com hortelã", nome: "suco de abacaxi com hortelã", Preço: 10.99 },
+    {Id: "heineken", nome: "heineken", Preço: 15.99 }
 ];
 
 
